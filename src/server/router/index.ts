bb03b078e44013bff42f0e08cc2859b6a5d7d768
Luis Marsiglia <marsiglia.business@gmail.com>
2022-07-16 01:21:33 -0400

    fix: :coffin: remove .home trpc route

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -1,12 +1,10 @@
 import superjson from 'superjson'
 import { createRouter } from './context'
-import { homeRouter } from './home'
 import { authRouter } from './auth'
 import { chatRouter } from './chat'
 
 export const appRouter = createRouter()
 	.transformer(superjson)
-	.merge('home.', homeRouter)
 	.merge('auth.', authRouter)
 	.merge('chat.', chatRouter)
 
